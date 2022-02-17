aff5f2b8ec8d259529569bd1de53a6794218a098
Jakub Wasilewski <98490230+WasilewskiJakub@users.noreply.github.com>
2022-02-17 19:47:10 +0100

    Lab10
    - Lambdas
    - Delegates
    - Extension Methods
    - IEnumerable<T>
    - List<T>
    
    Task is in polish language.

---
+++ Programowanie_C#/Lab10/Functions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LAB_2021_PL_10B
+{
+    //Stage 1 - 1.5 points
+    public static class DictionaryExtender
+    {
+       public static void AddElementsFromFunction<T>(this Dictionary<int, T> dict, int n, Func<(int,T)> f )
+        {
+            for(int i = 0; i <n;i++)
+            {
+                var x = f();
+                dict.Add(x.Item1, x.Item2);
+            }
+        }
+    }
+
+    //Stage 2 - 1 point
+    public static class Generators
+    {
+        static int iter = 0;
+        public static Func<int> NextInteger (int startingValue, int increaseValue)
+        {
+            return delegate() { return startingValue + iter++ * increaseValue; };
+        }
+        public static Func<int,T> LookUpKey<T>(IDictionary<int, T> diction, int keyIncreaseValue)
+        {
+            return delegate (int key)
+            {
+                if (diction.ContainsKey(key))
+                {
+                    T value = diction[key];
+                    diction.Remove(key);
+                    diction.Add(key + keyIncreaseValue, value);
+                    return value;
+                }
+                diction.Add(key, default(T));
+                return default(T);
+            };
+        }
+    }
+
+    //Stage 3 - 1 point
+    public static class FunctionsManipulator
+    {
+        public static Func<double, double> Distance(Func<double, double> f1, Func<double, double> f2) =>
+            (double x) => Math.Abs(f1(x) - f2(x));
+
+        public static Func<double, double, double> Integral(Func<double, double> f1) =>
+            (double x1, double x2) => (f1(x2) - f1(x1)) * (x2 - x1);
+    }
+
+
+    //Stage 4 - 1.5 points
+    public static class ExtensionMethods
+    {
+      public static (IEnumerable<T>, IEnumerable<T>) Partition<T>(this IEnumerable<T> list, Func<T,bool> f)
+        {
+            List<T> @false = new List<T>();
+            List<T> @true = new List<T>();
+            foreach(var elem in list)
+            {
+                if (f(elem))
+                    @true.Add(elem);
+                else
+                    @false.Add(elem);
+            }
+            return (@true, @false);
+        }
+        public static List<T> ReplaceIf<T>(this IEnumerable<T> lista, Func<T, bool> chooseFunc, Func<T, T> replaceFunc)
+        {
+            List<T> chosen = new List<T>();
+            foreach(T elem in lista)
+            {
+                if (chooseFunc(elem))
+                    chosen.Add(replaceFunc(elem));
+                else
+                    chosen.Add(elem);
+            }
+            return chosen;
+        }
+    }
+    
+}
---
+++ Programowanie_C#/Lab10/Program.cs
@@ -0,0 +1,157 @@
+﻿#define STAGE1
+#define STAGE2
+#define STAGE3
+#define STAGE4
+
+using System;
+using System.Collections.Generic;
+using System.Xml.XPath;
+
+
+namespace LAB_2021_PL_10B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stage 1 - 1.5 point
+#if STAGE1
+            {
+                Console.WriteLine("STAGE 1");
+                var fib = new Dictionary<int, int>();
+                //Uzupełnij
+                int fib1 = 1, fib2 = 1, i = 1;
+                fib.AddElementsFromFunction(10,()=>
+                { 
+                    if (i <= 2)
+                        return (i++, 1);
+                    int x = fib1 + fib2;
+                    fib1 = fib2;
+                    fib2 = x;
+                    return (i++, x);
+                });
+                Console.Write("Dictionary: ");
+                PrintIEnumerable(fib);
+
+                var ints = new List<int> {10, -5, 0, 4, 6, -6, 2321, -2452, 342, -67, 2};
+                //Wykorzystaj funkcję lambda
+                ints.Sort((x,y) => {
+                    if (Math.Abs(x) == Math.Abs(y))
+                        return x > 0 ? -1 : 1;
+                    return Math.Abs(x) > Math.Abs(y) ? -1 : 1;
+                });
+                Console.Write("Sorted list: ");
+                PrintIEnumerable(ints);
+                Console.WriteLine("\n\n");
+            }
+#endif
+            //Stage 2 - 1 point
+#if STAGE2
+            {
+                Console.WriteLine("STAGE 2");
+                var randomInt = Generators.NextInteger(5, 13);
+                Console.WriteLine("Next integer");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write(randomInt() + ", ");
+                }
+
+                Console.WriteLine("\n\n");
+
+
+                Dictionary<int, double> doubles = new Dictionary<int, double>
+                {
+                    {0, 0.2}, {1, 5.6f}, {2, 2}, {3, 4}, {4, -24}, {5, 16.2134124}, {6, 22}, {7, 82}, {8, 8.6},
+                    {9, 12.4}
+                };
+                var lookUpKey = Generators.LookUpKey(doubles, 1000);
+                Console.WriteLine("LookUpKey");
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine((i, lookUpKey(i)));
+                }
+
+                Console.WriteLine((1000, lookUpKey(1000)));
+                Console.WriteLine("\n\n");
+            }
+#endif
+            //Stage 3 - 1 point
+#if STAGE3
+            {
+                Console.WriteLine("Stage 3");
+                //f = |x-5|
+                Func<double,double> f = (x) => Math.Abs(x - 5);
+                //f = sin(x)*1/10*x
+                Func<double, double> g = (x) => Math.Sin(x) * 1 / 10 * x;
+                Console.WriteLine("Distance");
+                var distance = FunctionsManipulator.Distance(f, g);
+                for (int i = -5; i <= 5; i++)
+                {
+                    Console.WriteLine($"x: {i}, f(g(x)): {distance(i)}");
+                }
+
+                Console.WriteLine("\n\n");
+
+                //f = x ^ 3 + x ^ 2 - 5x + 3
+                f = (x) => Math.Pow(x, 3) + Math.Pow(x, 2) - 5 * x + 3;
+                var integral = FunctionsManipulator.Integral(f);
+                Console.WriteLine("Integral");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"f({i}): {f(i)}, f({2 * i}): {f(2 * i)}, integral from {i} to {2 * i}: {integral(i, 2 * i)}");
+                }
+
+                Console.WriteLine("\n\n");
+            }
+#endif
+            //Stage 4 - 1.5 points
+#if STAGE4
+            {
+                Console.WriteLine("STAGE 4");
+                Random random = new Random(0);
+                Console.WriteLine("Partition extension");
+                var ints = new List<int> {10, -5, 0, 4, 6, 2321, -2452, 342, -67, 2};
+                PrintIEnumerable(ints);
+
+                //Uzupełnij
+                int absSum = 0;
+                var tuple = ints.Partition((x)=>
+                {
+                    absSum += Math.Abs(x);
+                    return x % 2 == 0;
+                    });
+                Console.WriteLine($"Even");
+                PrintIEnumerable(tuple.Item1);
+                Console.WriteLine($"Odd");
+                PrintIEnumerable(tuple.Item2);
+                Console.WriteLine($"Sum of absolute values: {absSum}");
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("ReplaceIf extension");
+                var doubles = new List<double>();
+                for (int i = 0; i < 10; i++)
+                {
+                    doubles.Add(random.NextDouble() * 20);
+                }
+
+                PrintIEnumerable(doubles);
+
+                //Uzupełnij
+                var replacedDoubles = doubles.ReplaceIf((x) => x - (int)x > 0.5,
+                    (x)=>x/2 - (x - (int)x));
+                PrintIEnumerable(replacedDoubles);
+            }
+#endif
+        }
+
+        private static void PrintIEnumerable<T>(IEnumerable<T> ints)
+        {
+            foreach (var i in ints)
+            {
+                Console.Write(i + ", ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
