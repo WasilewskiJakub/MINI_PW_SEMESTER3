0bc9744439bfdc93709fed76c8d605d049824726
Jakub Wasilewski <98490230+WasilewskiJakub@users.noreply.github.com>
2022-02-19 18:48:53 +0100

    Lab13
    The task is to prepare a class representing the scoreboard that holds information about ski jumping tournaments.
    The table of results can store data both in the operating memory as well as save them properly on the computer disk.
    The task based on I/O streams .

---
+++ Programowanie_C#/Lab13/Person.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections;
+
+namespace Lab_12
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public List<Prize> Prizes { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}";
+        }
+    }
+    [Serializable]
+    public class Prize
+    {
+        public int Place { get; set; }
+        public Contest Contest { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Place} at {Contest}";
+        }
+    }
+    [Serializable]
+    public class Contest
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        [XmlIgnore]
+        public List<Person> Participants { get; set; }
+        public override string ToString()
+        {
+            return $"{Name} took place on {Date}";
+        }
+    }
+
+    public class PathAlreadyExistsException : Exception
+    {
+        private string file_path { get; set; }
+        public PathAlreadyExistsException(string file_path):base($"ERROR: {file_path} Exist.")
+        {
+            this.file_path = file_path;
+        }
+    }
+
+    public class Scoreboard : IEnumerable
+    {
+        public List<Person> participants { get; set; } = new List<Person>();
+        public List<Contest> tournaments { get; set; } = new List<Contest>();
+
+        public string File_Path { get; set; }
+        DirectoryInfo DirectoyToSave = null;
+        public Scoreboard(string path = "")
+        {
+            this.File_Path = path;
+            if(path!="")
+            {
+                if (Directory.Exists(path))
+                    throw new PathAlreadyExistsException(path);
+                else
+                {
+                    this.DirectoyToSave = Directory.CreateDirectory(path);
+                }
+            }
+        }
+        public void Add(Contest tournament)
+        {
+            var nameoftournament = Path.Combine(this.File_Path, $"{tournament.Name}.xml");
+            if (File.Exists(nameoftournament) == true) throw new PathAlreadyExistsException(nameoftournament);
+            XmlSerializer xmlserialize = new XmlSerializer(typeof(Contest));
+            using (Stream file = File.Create(nameoftournament))
+                xmlserialize.Serialize(file, tournament);
+            tournaments.Add(tournament);
+        }
+        public void Info()
+        {
+            foreach(var person in this.participants)
+            {
+                Console.WriteLine(person);
+                foreach(var prize in person.Prizes)
+                {
+                    Console.WriteLine(prize);
+                }
+            }
+            foreach(var tournam in tournaments)
+            {
+                Console.WriteLine(tournam);
+            }
+        }
+
+        public void Update(Contest ContentToChange)
+        {
+            try
+            {
+                Delete(ContentToChange.Name);
+            }
+            catch(Exception)
+            {
+                return;
+            }
+            Add(ContentToChange);
+        }
+        public bool Delete(string name)
+        {
+            var path = Path.Combine(this.File_Path, $"{name}.xml");
+            int idx = this.tournaments.FindIndex((x) => x.Name == name);
+            if(idx == -1)
+            {
+                if (File.Exists(path) == false) throw new PathAlreadyExistsException(path);
+            }
+            else
+            {
+                this.tournaments.RemoveAt(idx);
+            }
+            File.Delete(path);
+            return true;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach(string iterfile in Directory.EnumerateFiles(this.File_Path))
+            {
+                using (StreamReader reader = new StreamReader(iterfile))
+                {
+                    var deserializer = new XmlSerializer(typeof(Contest));
+                    yield return deserializer.Deserialize(reader) as Contest;
+                }
+            }
+        }
+        public static Scoreboard Create(string sourceFileContests)
+        {
+            if (File.Exists(sourceFileContests) == false)
+                throw new FileNotFoundException();
+            Scoreboard toret = new Scoreboard();
+            using(FileStream file = new FileStream(sourceFileContests,FileMode.Open))
+            {
+                BinaryFormatter serializer = new BinaryFormatter();
+#pragma warning disable SYSLIB0011
+                object x = serializer.Deserialize(file);
+#pragma warning restore SYSLIB0011
+                toret.tournaments = x as List<Contest>;
+            }
+            foreach (var tourn in toret.tournaments)
+            {
+                foreach (var person in tourn.Participants)
+                    if(toret.participants.Contains(person)==false)
+                        toret.participants.Add(person);
+            }
+            return toret;
+        }
+        public void Save(string directoryPath)
+        {
+            using(FileStream file = new FileStream(directoryPath,FileMode.Create))
+            {
+                BinaryFormatter serialize = new BinaryFormatter();
+                serialize.Serialize(file, this.participants);
+            }
+        }
+    }
+}
---
+++ Programowanie_C#/Lab13/Program.cs
@@ -0,0 +1,156 @@
+﻿#define ETAP1
+#define ETAP2
+//#define ETAP3
+#define ETAP4
+
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Lab_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string scoreboardPath = Path.Combine(Directory.GetCurrentDirectory(), "Scoreboard");
+
+            Prize prize1 = new Prize() { Place = 1 };
+            Prize prize2 = new Prize() { Place = 2 };
+            Prize prize3 = new Prize() { Place = 3 };
+            Prize prize4 = new Prize() { Place = 1 };
+            Prize prize5 = new Prize() { Place = 2 };
+            Prize prize6 = new Prize() { Place = 3 };
+
+            Person participant1 = new Person()
+            {
+                Name = "Adam",
+                Surname = "Malysz",
+                Birthday = DateTime.Parse("3.12.1977"),
+                Prizes = new List<Prize> { prize1, prize3, prize4 },
+            };
+
+            Person participant2 = new Person()
+            {
+                Name = "Kamil",
+                Surname = "Stoch",
+                Birthday = DateTime.Parse("25.05.1987"),
+                Prizes = new List<Prize> { prize2, prize1 },
+            };
+
+            Person participant3 = new Person()
+            {
+                Name = "Piotr",
+                Surname = "Ahonen",
+                Birthday = DateTime.Parse("16.01.1987"),
+                Prizes = new List<Prize> { prize4, prize5, prize6 },
+            };
+
+            Person participant4 = new Person()
+            {
+                Name = "Janne",
+                Surname = "Zyla",
+                Birthday = DateTime.Parse("11.05.1977"),
+                Prizes = new List<Prize> { prize5, prize2 },
+            };
+
+            Contest contest1 = new Contest()
+            {
+                Date = DateTime.Parse("15.12.2016"),
+                Name = "Turniej 4 Skoczni",
+                Participants = new List<Person>() { participant1, participant3, participant4 }
+            };
+
+            Contest contest2 = new Contest()
+            {
+                Date = DateTime.Parse("17.02.2017"),
+                Name = "Sapporo Contest",
+                Participants = new List<Person>() { participant2, participant1, participant3 }
+            };
+
+            Contest contest3 = new Contest()
+            {
+                Date = DateTime.Parse("28.01.2017"),
+                Name = "Villingen Contest",
+                Participants = new List<Person>() { participant1, participant2, participant3, participant4 }
+            };
+
+            prize1.Contest = contest1;
+            prize2.Contest = contest2;
+            prize3.Contest = contest3;
+            prize4.Contest = contest3;
+            prize5.Contest = contest1;
+            prize6.Contest = contest2;
+
+
+            Console.WriteLine("------------------ Etap1 ------------------");
+#if ETAP1
+            if (Directory.Exists(scoreboardPath) == true)
+            {
+                Directory.Delete(scoreboardPath, true);
+            }
+
+            Scoreboard scoreboard = new Scoreboard(scoreboardPath);
+
+            scoreboard.Add(contest1);
+            scoreboard.Add(contest2);
+            scoreboard.Add(contest3);
+
+            try
+            {
+                scoreboard.Add(contest3);
+            }
+            catch (PathAlreadyExistsException)
+            {
+                Console.WriteLine("Exception Expected: OK!");
+            }
+
+            scoreboard.Info();
+#endif
+            Console.WriteLine("------------------ Etap2 ------------------");
+#if ETAP2
+            {
+                contest3.Date = DateTime.Parse("29.11.2019");
+
+                scoreboard.Update(contest3);
+
+                scoreboard.Info();
+
+                Console.WriteLine();
+
+                scoreboard.Delete(contest3.Name);
+
+                foreach (Contest contest in scoreboard)
+                    Console.WriteLine(contest);
+            }
+#endif
+            Console.WriteLine("------------------ Etap3 ------------------");
+#if ETAP3
+            {
+                if (Directory.Exists(scoreboardPath) == true)
+                {
+                    Directory.Delete(scoreboardPath, true);
+                }
+
+                Scoreboard newScoreboard = Scoreboard.Create("./Contests.bin");
+
+                newScoreboard.Info();
+            }
+#endif
+
+            Console.WriteLine("------------------ Etap4 ------------------");
+#if ETAP4
+            {
+                string pathFilename = Path.Combine(Directory.GetCurrentDirectory(), "Participants.bin");
+
+                scoreboard.Save(pathFilename);
+
+                if (File.Exists(pathFilename) == true) Console.WriteLine("Binary File Found: OK!");
+                else Console.WriteLine("Binary File NOT Found: Error!");
+            }
+#endif
+        }
+    }
+}
